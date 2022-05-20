6eeadf51b2e43feefa1e6a1a146a00158272ab58
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-19 21:34:22 -0400

    simplify regex

--- fzf-url.sh
+++ fzf-url.sh
@@ -31,13 +31,13 @@ items="$(
 echo "$content" |
     perl -0pe 's@-\s*\n\s*\d*\s*@-@g' |
     perl -MList::Util=uniq -e '
-@l=reverse<>; @m=(); $c=0;
-do{
-    push (@m, $&) while m{\b(?:https?|ftp|file|git|ssh):/?//[-A-Za-z0-9+&@#/%?=~_|!:,.;]*[-A-Za-z0-9+&@#/%=~_|]\b}g;
-    push (@m, "http://$2") while m{(^|\s)([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}(?::[0-9]{1,5})?(?:/\S+)*)\b}g;
-} for @l;
-printf "%3d  %s\n", ++$c, $_ for uniq(@m)
-'
+    @l=reverse<>; @m=(); $c=0;
+    do{
+        push @m, $& while m{\b(?:https?|ftp|file|git|ssh):/?//[-A-Za-z0-9+&@#/%?=~_|!:,.;]*[-A-Za-z0-9+&@#/%=~_|]\b}g;
+        push @m, "http://$1" while m{\b([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}(?::[0-9]{1,5})?(?:/\S+)*)\b}g;
+    } for @l;
+    printf "%3d  %s\n", ++$c, $_ for uniq(@m)
+    '
 )"
 
 [[ -z "$items" ]] && exit 0
